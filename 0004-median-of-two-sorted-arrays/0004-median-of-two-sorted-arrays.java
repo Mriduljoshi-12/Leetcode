8ad255ba6904c89f6dd743e6bda529c609f0eeed
Mridul joshi <jmridul138@gmail.com>
2026-08-21 11:55:43 +0530

    Time: 2 ms (46.81%), Space: 48.9 MB (52.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,19 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-         int m= nums1.length;
-         int n= nums2.length;
-         int arr[]= new int[m+n];
-         int l= m+n;
-         int i=0,j=0,k=0;
-         while (i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                arr[k]=nums1[i];
-                k++;
-                i++;
-            }
-            else{
-                arr[k]=nums2[j];
-                k++;
-                j++;
-            }
-         }
-
-         while (i<m){
-            arr[k]=nums1[i];
-            k++;
-            i++;
-         }
-         while (j<n){
-            arr[k]=nums2[j];
-            k++;
-            j++;
-         }
-        if(l%2==0){
-            return ( arr[l/2 -1] + arr[l/2] ) /2.0;
+        int n1= nums1.length;
+        int n2=nums2.length;
+        int arr[]=new int[n1+n2];
+        int i=0,j=0,k=0;
+        while(i<n1 &&j<n2){
+            if (nums1[i]<nums2[j]) arr[k++]=nums1[i++];
+            else arr[k++]=nums2[j++];
         }
-        else {
-            return arr[l/2];
+        while(i<n1) arr[k++]=nums1[i++];
+        while (j<n2) arr[k++]=nums2[j++];
+        int n=n1+n2;
+        if(n%2==1){
+            return arr[n/2];
         }
+        else return ((arr[n/2 -1] + arr[n/2] ) /2.0);
     }
 }
