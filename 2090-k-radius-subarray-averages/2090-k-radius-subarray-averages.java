a87337970fd3f2bb9d7e5fd2ad388df5ea34a0bd
Mridul joshi <jmridul138@gmail.com>
2026-09-08 21:13:27 +0530

    Time: 2137 ms (5.1%), Space: 174.9 MB (75.13%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -5,21 +5,19 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i]=-1;
         }
-        long sum=0;
-        for(int i=0;i<((2*k)+1)&& i<n;i++){
-            sum+=nums[i];
+        if (2 * k + 1 > n) {
+            Arrays.fill(arr, -1);
+            return arr;
         }
         for(int i=k;i<n-k;i++){
+            long sum=0;
             long avg=0;
-            if(i==k){
-                avg=(int)(sum/((2*k)+1));
-            }
-            else{
-                sum= sum -nums[i-k-1] +nums[i+k];
-                avg=(int)(sum/((2*k)+1));
-            }
-            arr[i]=(int)avg;
-        }
+                for(int j=i-k;j<=i+k;j++){
+                    sum+=nums[j];
+                }
+        avg=(sum/((2*k)+1));
+        arr[i]=(int)avg;
+        }   
         return arr;
     }
 }
